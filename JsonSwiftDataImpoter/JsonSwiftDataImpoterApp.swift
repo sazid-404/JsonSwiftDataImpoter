551df85d919a1afbbae91e2ba849a6f788c742e5
sazid-404 <sazzadulislamsazid@gmail.com>
2025-09-15 12:25:28 -0700

    Initial Commit

---
+++ JsonSwiftDataImpoter/JsonSwiftDataImpoterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonSwiftDataImpoterApp.swift
+//  JsonSwiftDataImpoter
+//
+//  Created by Sazzadul Islam on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct JsonSwiftDataImpoterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
